ee4e4ce2cd3f806d3927eaea57dfabd233c7d61e
Yashvi <yashvi198mudgal@gmail.com>
2025-06-10 15:16:52 +0530

    Product related APIs

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
-#from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
 engine = create_engine(settings.DATABASE_URL)
-
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
